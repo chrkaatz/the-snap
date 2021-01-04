b8d710480b7986857bc3ff695ac742ce26a61a78
Christian Kaatz <chrkaatz@googlemail.com>
2021-01-04 21:31:52 +0100

    no fail

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-04T20:26:56.013Z",
+    "time": "2021-01-04T20:31:52.797Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
