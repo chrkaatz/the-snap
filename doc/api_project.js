03da6b3d8d54c74f534d106366923330028e2313
Christian Kaatz <chrkaatz@googlemail.com>
2021-01-04 21:26:55 +0100

    Remove is-ci package

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-04T20:14:20.316Z",
+    "time": "2021-01-04T20:26:56.013Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
