887ba1cea3cb50b1406eecce5acc4f61af966bc3
Christian Kaatz <chrkaatz@googlemail.com>
2021-01-04 21:14:19 +0100

    Git existence

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-04T20:10:26.632Z",
+    "time": "2021-01-04T20:14:20.316Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
