a8c905175afcf3b6bd9d0640d2b1b9641e2b2d4d
Christian Kaatz <chrkaatz@googlemail.com>
2021-01-29 23:07:12 +0100

    Flexible ratelimit and update license

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-29T21:32:58.656Z",
+    "time": "2021-01-29T22:07:13.456Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }
--- src/middleware/rate-limit.js
+++ src/middleware/rate-limit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 const limiter = () =>
   rateLimit({
     windowMs: 60 * 60 * 1000, // 15 minutes
-    max: 20, // limit each IP to 100 requests per windowMs
+    max: process.env.RATE_LIMIT || 20, // limit each IP to 100 requests per windowMs
   });
 
 module.exports = limiter;
